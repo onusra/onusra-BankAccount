f2f973e0d3bcb74092896ae087f02302ac96bfba
onusra <onusra@angelo.edu>
2023-02-10 20:46:15 +0000

    Bank account min

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        this.balance += amount;
     }
 
     /**
@@ -32,7 +32,11 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if((this.balance - amount) < 0){
+            //throw new ArithmeticException("Balance cannot be negative");
+        }else{
+            this.balance -= amount;
+        }
     }
 
     /**
@@ -40,6 +44,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return this.balance;
     }
 }
